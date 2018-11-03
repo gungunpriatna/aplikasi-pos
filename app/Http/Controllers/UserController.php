8be49c0348223f55df6983c3fdce8a74e0c3dfb1
Gun Gun Priatna <gungunpriatna02@gmail.com>
2018-11-03 08:54:33 +0700

    role and permission management

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Spatie\Permisssion\Models\Role;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 use App\User;
+use DB;
 
 class UserController extends Controller
 {
@@ -70,5 +72,79 @@ class UserController extends Controller
         $user = User::findOrFail($id);
         $user->delete();
         return redirect()->back()->with(['success' => 'User: <b>' . $user->name . '</b> Dihapus']);
+    }
+
+    public function rolePermission(Request $request)
+    {
+        $role = $request->get('role');
+
+        //default
+        $permissions = null;
+        $hasPermission = null;
+
+        //ambil data role
+        $roles = Role::all()->pluck('name');
+
+        //apabila parameter role terpenuhi
+        if (!empty($role)) {
+            //select role
+            $getRole = Role::findByName($role);
+
+            //Query unntuk mengambil permisssion
+            $hasPermission = DB::table('role_has_permissions')
+                ->select('permissions.name')
+                ->join('permissions', 'role_has_permissions.permission_id', '=', 'permissions.id')
+                ->where('role_id', $getRole->id)->get()->pluck('name')->all();
+
+            //ambil data permission
+            $permissions = Permission::all()->pluck('name');
+        }
+
+        return view('users.role_permission', compact('roles', 'permissions', 'hasPermission'));
+    }
+
+    public function addPermission(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|unique:permissions'
+        ]);
+
+        $permission = Permission::firstOrCreate([
+            'name' => $request->name
+        ]);
+
+        return redirect()->back();
+    }
+
+    public function setRolePermission(Request $request, $role)
+    {
+        //select
+        $role = Role::findByName($role);
+
+        //fungsi syncPermission() akan menghapus semua permission yang dimiliki role tersebut
+        //kemudian di-assing kembali sehingga tidak terjadi duplicate data
+        $role->syncPermissions($request->permission);
+        return redirect()->back()->with(['success' => 'Permission to Role saved!']);
+    }
+
+    public function roles(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $roles = Role::all()->pluck('name');
+        return view('users.roles', compact('user', 'roles'));
+    }
+
+    public function setRole(Request $request, $id)
+    {
+        $this->validate($request, [
+            'role' => 'required'
+        ]);
+
+        $user = User::findOrFail($id);
+
+        //menggunakan syncRoles agar terlebih dahulu menghapus semua role yang dimiliki
+        //kemudian di-set kembali agar tidak terjadi duplicate
+        $user->syncRoles($request->role);
+        return redirect()->back()->with(['success' => 'Role sudah di set']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,41 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function() {
-    Route::resource('/kategori', 'CategoryController')->except([
-        'create', 'show'
-    ]);
+    //Route untuk role admin
+    Route::group(['middleware' => ['role:admin']], function() {
+        Route::resource('/role', 'RoleController')->except([
+            'create', 'show', 'edit', 'update'
+        ]);
 
-    Route::resource('produk', 'ProductController');
+        Route::resource('/users', 'UserController')->except([
+            'show'
+        ]);
+        Route::get('/users/roles/{id}', 'UserController@roles')->name('users.roles');
+        Route::put('/users/roles/{id}', 'UserController@setRole')->name('users.set_role');
+        Route::post('/users/permission', 'UserController@addPermission')->name('users.add_permission');
+        Route::get('/users/role-permission', 'UserController@rolePermission')->name('users.roles_permission');
+        Route::put('/users/permission/{role}', 'UserController@setRolePermission')->name('users.setRolePermission');
+        
+    });
+
+    //route yang bisa diakses oleh user yang memiliki permission yang
+    //telah disebutkan di bawah
+    Route::group(['middleware' => ['permission:show products|create products|delete products']], function(){
+        Route::resource('/kategori', 'CategoryController')->except([
+            'create', 'show'
+        ]);
+        Route::resource('produk', 'ProductController');
+        
+    });
+
+    //route untuk kasir
+    Route::group(['middleware' => ['role:kasir']], function() {
+
+    });
+
+    
+
+    
 
 
     Route::get('/home', 'HomeController@index')->name('home');
@@ -30,11 +60,5 @@ Route::group(['middleware' => 'auth'], function() {
 });
 
 
-Route::resource('/role', 'RoleController')->except([
-    'create', 'show', 'edit', 'update'
-]);
 
-Route::resource('/users', 'UserController')->except([
-    'show'
-]);
-Route::get('/users/roles/{id}', 'UserController@roles')->name('users.roles');
+
